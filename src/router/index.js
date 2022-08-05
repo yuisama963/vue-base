3bf22ec69c848d06d0532f8bcddcf2069cd9ddf6
pwl <18965130782@189.cn>
2022-08-05 18:51:06 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-06-09 18:08:47
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-08-04 17:46:04
+ * @LastEditTime: 2022-08-05 18:50:49
  * @FilePath: \basic\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -124,7 +124,7 @@ export const publicRoutes = [
           import('@/views/sys-mgmt/menu-mgmt/index.vue'),
         meta: {
           title: '菜单管理',
-          icon: 'personnel-manage'
+          icon: 'personnel-manage'                                                                        
         }
       }
     ]
