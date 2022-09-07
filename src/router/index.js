87a8937f21565bcdae3e0e35d053357341ff5b1f
pwl <18965130782@189.cn>
2022-09-07 20:01:08 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-06-09 18:08:47
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-09-06 16:45:37
+ * @LastEditTime: 2022-09-07 19:40:30
  * @FilePath: \basic\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -172,7 +172,7 @@ export const publicRoutes = [
         component: () =>
           import('@/views/user-ctr/owner-mgmt/detail.vue'),
         meta: {
-          title: '注册货主',
+          title: '货主主页',
           id: 'registerOwner'
         }
       },
--- src/views/user-ctr/owner-mgmt/detail.vue
+++ src/views/user-ctr/owner-mgmt/detail.vue
@@ -2,21 +2,32 @@
  * @Author: error: git config user.name && git config user.email & please set dead value or install git
  * @Date: 2022-09-06 16:45:52
  * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-09-06 20:21:45
+ * @LastEditTime: 2022-09-07 19:58:21
  * @FilePath: \basic\src\views\user-ctr\owner-mgmt\detail.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
 <a-tabs
       v-model:activeKey="activeKey"
-      :tab-position="mode"
-      :style="{ height: '200px' }"
-      @tabScroll="callback"
     >
-      <a-tab-pane v-for="i in 30" :key="i" :tab="`Tab-${i}`">Content of tab {{ i }}</a-tab-pane>
+      <a-tab-pane :tab="`基础信息`" :key="1">
+      <div class="basic-info"></div>
+        <RegisterForm />
+        <a-switch></a-switch>
+      </a-tab-pane>
+      <a-tab-pane :tab="`参数配置`" :key="2">
+      <div class="param-setting"></div>
+      </a-tab-pane>
+      <a-tab-pane :tab="`功能配置`" :key="3">
+      <div class="func-setting"></div>
+      </a-tab-pane>
     </a-tabs>
 
 </template>
 
 <script setup>
+import registerForm from './components/registerForm.vue'
+import { ref } from 'vue'
+import RegisterForm from './components/registerForm.vue'
+const activeKey = ref(1)
 </script>
